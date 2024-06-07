9d639f8a1994c0b0c2b534b0c09a0ac0541164f9
abdul <abdul.tanveer@gmail.com>
2024-06-07 10:28:14 +0530

    convert java file to kotlin

--- app/src/main/java/com/example/vitbatch1/kotlinexs/Example.kt
+++ app/src/main/java/com/example/vitbatch1/kotlinexs/Example.kt
@@ -2,5 +2,13 @@ package com.example.vitbatch1.kotlinexs
 
 fun main() {
     println("hello world");
+    var myStudent: Student = Student("abdul",123,"bangalore")
+    println(myStudent.name)
+    myStudent.name = "ansari"
+    println(myStudent.name)
+
+    var myEmployee: Employee = Employee("tanveer",222,"chennai")
+    println(myEmployee.getName())
+    println(myEmployee.name)
 
 }
--- app/src/main/java/com/example/vitbatch1/kotlinexs/Student.kt
+++ app/src/main/java/com/example/vitbatch1/kotlinexs/Student.kt
@@ -1,40 +1,6 @@
-package com.example.vitbatch1.kotlinexs;
+package com.example.vitbatch1.kotlinexs
 
 /**
  * POJO - plain old java object / VO value object / model class -- box with partitionns
  */
-public class Student {
-    String name;
-    int age;
-    String postalAddress;
-
-    public Student(String name, int age, String postalAddress) {
-        this.name = name;
-        this.age = age;
-        this.postalAddress = postalAddress;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPostalAddress() {
-        return postalAddress;
-    }
-
-    public void setPostalAddress(String postalAddress) {
-        this.postalAddress = postalAddress;
-    }
-}
+class Student(var name: String, var age: Int, var postalAddress: String)
