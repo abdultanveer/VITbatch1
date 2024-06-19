1479527071d14f0266ebc8a734368f1df16a5ab5
abdul <abdul.tanveer@gmail.com>
2024-06-19 11:15:05 +0530

    parsing  json fix-- applied the converter plugin in  project gradle

--- app/src/main/java/com/example/vitbatch1/HomeActivity.kt
+++ app/src/main/java/com/example/vitbatch1/HomeActivity.kt
@@ -39,8 +39,8 @@ class HomeActivity : AppCompatActivity(){
         GlobalScope.launch {
 
             var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
-            var tvHome:TextView = findViewById(R.id.tvHome)
-            tvHome.setText(listMarsPhotos.get(1).imgSrc)
+         //   var tvHome:TextView = findViewById(R.id.tvHome)
+//            tvHome.setText(listMarsPhotos.get(1).imgSrc)
             Log.i("homeactiviy",listMarsPhotos.size.toString())
         }
     }
