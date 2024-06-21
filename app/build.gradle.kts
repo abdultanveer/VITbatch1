f8ab8bcfbcbf6695c411ca63da7dc2e1a427adf1
abdul <abdul.tanveer@gmail.com>
2024-06-21 10:22:22 +0530

    viewbinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,10 @@ android {
     namespace = "com.example.vitbatch1"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.vitbatch1"
         minSdk = 24
--- app/src/main/java/com/example/vitbatch1/HomeActivity.kt
+++ app/src/main/java/com/example/vitbatch1/HomeActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.vitbatch1.databinding.ActivityHomeBinding
 import com.example.vitbatch1.network.MarsApi
 import com.example.vitbatch1.network.MarsPhoto
 import kotlinx.coroutines.Dispatchers
@@ -18,24 +19,31 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.Dispatcher
 
+//wet code --- dry code -- repetative lines removed
 class HomeActivity : AppCompatActivity(){
     var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
 
-    lateinit var marsRecyclerView:RecyclerView
+    private lateinit var binding: ActivityHomeBinding
+
+    //lateinit var marsRecyclerView:RecyclerView
     lateinit var marsAdapter: MarsAdapter
     lateinit var photos:List<MarsPhoto>
-    lateinit var imageView: ImageView
+  //  lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        imageView = findViewById(R.id.imageView)
-        marsRecyclerView = findViewById(R.id.recyclerViewUrls)
-        marsRecyclerView.layoutManager = LinearLayoutManager(this)
+       // setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+       // imageView = findViewById(R.id.imageView)
+       // marsRecyclerView = findViewById(R.id.recyclerViewUrls)
+        binding.recyclerViewUrls.layoutManager = LinearLayoutManager(this)
         photos = ArrayList()
         marsAdapter = MarsAdapter(photos)
-        marsRecyclerView.adapter = marsAdapter
+        binding.recyclerViewUrls.adapter = marsAdapter
 
        // marsAdapter = MarsAdapter(photos)
 
@@ -60,6 +68,7 @@ class HomeActivity : AppCompatActivity(){
             marsAdapter.notifyDataSetChanged()
          //   var tvHome:TextView = findViewById(R.id.tvHome)
 //            tvHome.setText(listMarsPhotos.get(1).imgSrc)
+          //  binding.imageView.load()
             Log.i("homeactiviy",listMarsPhotos.size.toString())
             Log.i("homeactivity-url",listMarsPhotos.get(1).imgSrc)
 
