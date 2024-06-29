fa4d869a19775c99e5051015ae1a4c49a3754005
abdul <abdul.tanveer@gmail.com>
2024-06-29 12:04:12 +0530

    alarmmannager and sms sending

--- app/src/main/java/com/example/vitbatch1/HomeActivity.kt
+++ app/src/main/java/com/example/vitbatch1/HomeActivity.kt
@@ -1,29 +1,35 @@
 package com.example.vitbatch1
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ListView
 import android.widget.Spinner
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.Instant
 
-class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
-    AdapterView.OnItemClickListener {
+
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
     var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
     lateinit var mySpinner: Spinner
-    lateinit var myListview:ListView
+   // lateinit var myListview:ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         mySpinner = findViewById(R.id.spinner) //taking handle
-        myListview = findViewById(R.id.listView)
-        myListview.isClickable = true
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,7 +38,7 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
         }
 
         mySpinner.onItemSelectedListener = this
-        myListview.setOnItemClickListener(this)
+
         //get intent which sttarted this activity
         //get the extras from that intent
         //get the string with mykey
@@ -52,7 +58,26 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
         TODO("Not yet implemented")
     }
 
-    override fun onItemClick(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        var item:String = adpater?.getItemAtPosition(position).toString()
-        Log.i(TAG, item )    }
+    //runntime permissions
+    @SuppressLint("ScheduleExactAlarm")
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun scheduleSms(view: View) {
+        Log.i("HomeActivity","current system time in millis"+SystemClock.elapsedRealtime())
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val triggerTime = Instant.now().toEpochMilli() + 30 *1000
+            //System.currentTimeMillis()+30*60
+            //SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_FIFTEEN_MINUTES
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        //alarmManager.canScheduleExactAlarms()
+        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME,triggerTime,pendingIntent)
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/vitbatch1/MainActivity.kt
+++ app/src/main/java/com/example/vitbatch1/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.vitbatch1
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.vitbatch1.kotlinexs.Employee
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     //activiy is getting created -- chick is in the egg -- mem being allocated for activity
@@ -24,38 +28,24 @@ class MainActivity : AppCompatActivity() {
        // var myEmp = Employee("abdul")
     }
 
-    //chick came out of the egg --activity is visible to the user to interact - click
     override fun onStart() {
         super.onStart()
-        Log.e(TAG,"activity is started")
-    }
-
-    //sleep night -- incomming call ur activity goes into the background for sometime
-    override fun onPause() {
-        super.onPause()
-        Log.w(TAG,"activity has paused")
-    }
-
-    //chick has woken up -- activity comes back to the foreground
-    override fun onResume() {
-        super.onResume()
-        Log.w(TAG,"activity has resumed")
-    }
+        var view:ConstraintLayout = findViewById(R.id.mainCl)
+        var snackbar = Snackbar.make(view,"about to send sms",Snackbar.LENGTH_SHORT)
+            .setAction("stop sending", View.OnClickListener {  })
+        snackbar.show()
 
 
-    //death of the chick -- activity no lonnger exists in the ram -- pushed to hdd[hibernate]
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG,"activity is stopped")
+        val smsManager: SmsManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.getSystemService(SmsManager::class.java)
+        } else {
+            SmsManager.getDefault()
+        }
+        smsManager.sendTextMessage("5556",null,"happy day",null,null)
 
     }
 
 
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v(TAG,"activity is destroyed")
-    }
 
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickhandler","button clicked")
